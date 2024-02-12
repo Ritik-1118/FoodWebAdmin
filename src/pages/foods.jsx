47d75819b27264dd8310c1180ae7b6dc153a3a11
Ritik <ritik11181@gmail.com>
2024-02-12 22:51:52 +0530

    Food page updated

--- src/pages/foods.jsx
+++ src/pages/foods.jsx
@@ -1,16 +1,22 @@
-import { NavLink } from "react-router-dom";
 import FoodCard from "../components/FoodCard"
 import { useEffect, useState } from "react";
 const Foods = () => {
-    const [foods,setFoods] = useState([]);
+    const [allfoods,setAllFoods] = useState([]);
+    const [viewAllFoods, setViewAllFoods] = useState(false);
+    const foods = viewAllFoods ? allfoods : allfoods.slice(0, 5);
+    const recommendedFood = foods.slice(0, 4);
+    
 
     const getAllFoods = async ()=>{
         const response = await fetch("http://localhost:8001/food/",{
             method:"GET",
         });
         const data = await response.json();
-        setFoods(data);
+        setAllFoods(data);
         console.log(data)
+    }
+    const showAllFoods = ()=>{
+        setViewAllFoods(true);
     }
     const deleteFood = async (id)=>{
         try {
@@ -69,19 +75,18 @@ const Foods = () => {
                             />
                         ))}
                 </div>
-                <NavLink
-                    to={'/search'}
-                    className='text-lg text-teal-500 hover:underline text-center'
-                >
-                    View all
-                </NavLink>
+                {!viewAllFoods && (
+                    <div onClick={showAllFoods} className='text-3xl text-teal-500 hover:underline text-center cursor-pointer'>
+                        View all
+                    </div>
+                )}
             </div>
 
             {/**Recommend Foods */}
             <div className='flex flex-col gap-6'>
                 <h2 className='text-4xl font-semibold text-center'>Recommend Foods</h2>
                 <div className='w-full items-center justify-center flex flex-wrap gap-4'>
-                        {foods.map((food) => (
+                        {recommendedFood.map((food) => (
                             <FoodCard 
                                 key={food._id} 
                                 food={food} 
@@ -92,12 +97,6 @@ const Foods = () => {
                             />
                         ))}
                 </div>
-                <NavLink
-                    to={'/search'}
-                    className='text-lg text-teal-500 hover:underline text-center'
-                >
-                    View all
-                </NavLink>
             </div>
         </div>
     </>
